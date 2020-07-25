e1553677bd64e5812cdf1c9b627c6ddcf2b99a52
omarsagoo <om.sagoo@gmail.com>
2020-07-25 16:19:17 -0700

    [add] patients form that uploads into DB

--- server/server.js
+++ server/server.js
@@ -3,8 +3,13 @@ let app = express()
 let patient = require("./models/patients")
 var bodyParser = require("body-parser");
 
+var urlEncodedParser = bodyParser.urlencoded({extended:false})
 
 app.use(bodyParser.json())
+app.use(express.static( '/Users/omarsagoo/dev/intensive/reminder-app/templates'));
+app.set('views', '/Users/omarsagoo/dev/intensive/reminder-app/templates');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
 app.get("/patients", function (req, res)  {
     patient.getPatients("1234124").then(function (response) {
@@ -12,11 +17,12 @@ app.get("/patients", function (req, res)  {
     })
 })
 
-app.post("/add/patient", function (req, res) {
+app.post("/add/patient", urlEncodedParser ,function (req, res) {
     console.log("here!", req.body)
-    // body = req.body
-    // console.log(body)
-    // patient.addPatients("1234124", )
+    patient.addPatients("1234124", req.body).then(function (response) {
+        console.log(response)
+        return res.redirect('/')
+    })
 })
 
 app.get("/", function (req, res) {
